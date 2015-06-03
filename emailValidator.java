5f006849f61638086d3c90aafd19afd243bfc627
Angelos Hadjiphilippou <angelos@exevior.com>
2015-06-03 11:41:29 +0300

    Update emailValidator.java

--- emailValidator.java
+++ emailValidator.java
@@ -2,17 +2,24 @@
 /*
 * Insert this where needed (e.g. OnCreate) and make appropriate changes to match your code 
 */
+    //Change to match your submit button if any (Remove if not applicable)
+    loginButton = (Button) findViewById(R.id.loginButton);
+    loginButton.setEnabled(false); //Disables button until email is valid (See below)
+    
+    //Change emailText to match your input field name
     final EditText emailValidate = (EditText)findViewById(R.id.emailText);
         
+        //This will trigger everytime the field is changed
         emailValidate .addTextChangedListener(new TextWatcher() {
             public void afterTextChanged(Editable s) {
+                //Gets the inputed value of the field (the email address)
                 String email = emailValidate.getText().toString().trim();
                 if (emailValidator(email)) {
-                    emailValidate.setError(null);
-                    loginButton.setEnabled(true);
+                    emailValidate.setError(null); //Destroys the error in editText field (See below)
+                    loginButton.setEnabled(true); //Enables your submitButton
                 } else {
-                    emailValidate.setError(email+ " is not a valid email address");
-                    loginButton.setEnabled(false);
+                    emailValidate.setError(email+ " is not a valid email address"); // Displays an error bubble inside your editText field
+                    loginButton.setEnabled(false); //Disables button until email is valid
                 }
             }
 
